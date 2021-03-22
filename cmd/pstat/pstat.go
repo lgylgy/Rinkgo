83231397f47eab10d93055a7a53175e232641102
Loic Geffroy <loic.geffroy@masagroup.net>
2021-03-22 11:06:53 +0100

    add Dockerfile

--- cmd/pstat/pstat.go
+++ cmd/pstat/pstat.go
@@ -12,11 +12,11 @@ import (
 )
 
 func main() {
-	port := flag.Int("port", 10000, "The server port")
+	port := flag.Int("port", 5000, "The server port")
 	url := flag.String("url", "", "The database url")
 	flag.Parse()
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
